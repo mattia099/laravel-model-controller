ec5f83b9405c6d63e3ef6a1b4ece8436af08b725
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-04-21 15:30:43 +0200

    aggiunto css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <title>Home</title>
 </head>
 <body>
-  <ul>
-  @foreach ($movies as $movie)
-    <li>
-      <h1>{{ $movie['title'] }}</h1>
-      <p> {{ $movie['date']}} </p>
-      <span>{{ $movie['vote'] }}</span>
-    </li>    
-  @endforeach
+  <div class="container">
+    <ul>
+      @foreach ($movies as $movie)
+        <li>
+          <h1>{{ $movie['title'] }}</h1>
+          <p> {{ $movie['date']}} </p>
+          <span>{{ $movie['vote'] }}</span>
+        </li>    
+      @endforeach
   </ul>
+</div>
 </body>
 </html>
