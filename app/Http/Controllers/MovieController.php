64404a7cbc20d29f64df3e9d676ce9ed782a3531
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-04-21 14:56:51 +0200

    creato controller e collegato database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+
+        $movies = Movie::all();
+
+        dd($movies);
+
+        return view('home')->compact($movies);
+    }
 }
